5fb0bfe11629234a40a8f992442f4100479d2cff
Ryan White <r160k@aol.com>
2025-04-22 11:51:54 +0100

    Update wrapper.sh

--- wrappers/bash/wrapper.sh
+++ wrappers/bash/wrapper.sh
@@ -3,8 +3,15 @@
 EXE_PATH=cdls_ex
 #EXE_PATH = /bin/cdls_ex
 
+# Check if command-line arg has been passed with new EXE_PATH
+if [ $# -gt 0 ]; then
+    EXE_PATH=$1
+fi
+
+echo $EXE_PATH
+
 cdls() {
-	cd $(cdls_ex -w "$@" | tee /dev/tty | tail -n 1)
+	cd $($EXE_PATH -w "$@" | tee /dev/tty | tail -n 1)
 }
 
 export -f cdls
