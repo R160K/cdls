e25d0572bad3c7136cecc229ffd64171ab1497ea
Ryan White <r160k@aol.com>
2025-04-22 18:26:30 +0100

    Added environment variable option to wrapper.sh

--- wrappers/bash/wrapper.sh
+++ wrappers/bash/wrapper.sh
@@ -6,6 +6,9 @@ EXE_PATH=cdls_ex
 # Check if command-line arg has been passed with new EXE_PATH
 if [ $# -gt 0 ]; then
     EXE_PATH=$1
+# Else check if the $CDLS_EXEC_PATH environment variable has been set
+elif [ $CDLS_EXE_PATH ]; then
+    EXE_PATH=$CDLS_EXE_PATH
 fi
 
 echo $EXE_PATH
